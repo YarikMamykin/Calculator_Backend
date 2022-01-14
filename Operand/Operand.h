983272770fe8495eb14df3d8ac43cc0e2e847a7e
YarikM <ymamykin@gmail.com>
2022-01-15 00:40:18 +0200

    Add Operand and Operation implementations
    Operation - class that represents mathematical operation like
    '+', '-', '/', '*' and its operands.
    Operand - class that represents operand of mathematical Operation.
    An Operand can be result of another operation.

---
+++ Operand/Operand.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <list>
+#include <variant>
+
+namespace operation {
+
+	struct Operation;
+
+	struct Operand {
+
+		Operand(std::list<double>::iterator value);
+		Operand(std::list<Operation>::iterator value);
+
+		double as_double() const;
+
+		bool is_number() const noexcept;
+
+		bool is_operation() const noexcept;
+
+		bool operator == (const Operand& o) const;
+
+		std::variant<std::list<double>::iterator, std::list<Operation>::iterator> value;
+	};
+
+}
