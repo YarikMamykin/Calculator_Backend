9864dec36d793042a548bc21806f8e850d022515
YarikM <ymamykin@gmail.com>
2022-01-15 01:07:24 +0200

    Implement main entry point

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <CalculationPipe.h>
+#include <Connection.h>
+#include <cstdlib>
+
+struct ConnectionClient : public connection::Connection::Client {
+    std::string on_data_receive(std::string&& data) override {
+        calculation::CalculationPipe pipe;
+        return std::to_string(pipe.run(std::move(data)));
+    }
+};
+
+std::string parse_host() {
+
+    if(const char* host = std::getenv("CALC_HOST")) {
+        return { host };
+    }
+
+    return { "127.0.0.1" };
+}
+
+unsigned short parse_port() { 
+
+    if(const char* port = std::getenv("CALC_PORT")) {
+        return std::stoi(port);
+    }
+
+    return 8081;
+}
+
+int main(int argc, char* argv[])
+{
+    const std::string host  = parse_host();
+    const unsigned short port = parse_port();
+
+    connection::Connection c ({ host, port, 1});
+    auto client = std::make_shared<ConnectionClient>();
+    c.set_client(client);
+
+    std::cout << "Listening on " << host << ":" << port << "\n";
+    c.poll();
+
+    return 0;
+}
