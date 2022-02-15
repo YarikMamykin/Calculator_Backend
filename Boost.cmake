02b05d4419f9107a25788d25e6a997b2eefd5087
Yaroslav Mamykin (GitHub) <33784535+YarikMamykin@users.noreply.github.com>
2022-02-15 12:50:15 +0200

    Speedup boost build (#18)
    * Add jobs number flag for Boost build
    
    * Update CI config
    
    * fixup! Add jobs number flag for Boost build

--- Boost.cmake
+++ Boost.cmake
@@ -2,19 +2,25 @@ cmake_minimum_required(VERSION 3.7)
 
 set(BOOST_INSTALL_DIR ${CMAKE_BINARY_DIR}/boost)
 
+if(NOT DEFINED BOOST_BUILD_CPU_THREADS)
+    set(BOOST_BUILD_CPU_THREADS 1)
+endif()
+
 include(ExternalProject)
 ExternalProject_Add(
     boost
 		URL https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz
     BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND "./bootstrap.sh"
-    BUILD_COMMAND "./b2" install 
-                        --prefix=${BOOST_INSTALL_DIR} 
-                        --threading=single,multi 
-                        --link=shared 
-                        --without-python 
-                        --without-mpi 
-                        --disable-icu
+    CONFIGURE_COMMAND "./bootstrap.sh" --prefix=${BOOST_INSTALL_DIR} 
+    BUILD_COMMAND "./b2" --disable-icu 
+                         --with-system 
+                         --with-thread 
+                         --with-date_time 
+                         --with-regex 
+                         --with-serialization 
+                         --link=shared 
+                         -j ${BOOST_BUILD_CPU_THREADS} 
+                         install 
     INSTALL_COMMAND ""
     INSTALL_DIR ${BOOST_INSTALL_DIR}
 )
